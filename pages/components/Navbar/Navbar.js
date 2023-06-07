7295a540a1ea9915f936b4a2aa43023804e33183
killyan <piriou.killyan@gmail.com>
2023-06-07 13:09:48 +0200

    start-nav

--- pages/components/Navbar/Navbar.js
+++ pages/components/Navbar/Navbar.js
@@ -3,8 +3,8 @@ import { SecNav } from "./SecNav";
 export function Navbar() {
     return (
         <nav>
-            <img src="../../../images/logo_1.png"/>
-            <div id="main-navbar">
+            <a href="#"><img src="../../../images/logo_1.png"/></a>
+            <div id="main-navbar" className="flex items-center w-2/3">
                 <a className="liens-nav" href="#">Accueil</a>
                 <a className="liens-nav" href="#">Imformation</a>
                 <a className="liens-nav" href="#">Galerie</a>
--- pages/components/Navbar/SecNav.js
+++ pages/components/Navbar/SecNav.js
@@ -1,6 +1,8 @@
 export function SecNav() {
     return (
-        <>
-        </>
+        <div>
+            <i>icon1</i>
+            <i>icon2</i>
+        </div>
     );
 }
